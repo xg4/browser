4c9f915cd34011ee08ae80bf7edbd5f2225c3ffc
xg4 <xingor4@gmail.com>
2019-11-26 11:15:34 +0800

    chore: rollup template

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
+import pkg from './package.json'
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx']
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs'
+      },
+      {
+        file: pkg.module,
+        format: 'es'
+      },
+      {
+        name: 'Browser',
+        file: pkg.browser,
+        format: 'umd'
+      }
+    ],
+    plugins: [
+      resolve({ extensions }),
+      commonjs(),
+      babel({
+        extensions,
+        include: ['src/**/*'],
+        exclude: 'node_modules/**'
+      }),
+      terser()
+    ]
+  }
+]
